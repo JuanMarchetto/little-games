b9745ba8872e06e250bdf85c7bdb20cc30ea8dbb
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2018-07-24 15:03:47 -0300

    alternative to modal

--- js/games-modal.js
+++ js/games-modal.js
@@ -1,9 +1,3 @@
-function gamesModal(game) {
-	$("body").append("<div id='gamesModal'></div>");
-  var url = game + "/index.html"
-  $("#gamesModal").load(url);
-}
-
 $( ".game" ).click(function(e) {
-	gamesModal(e.target.id);
+	window.location.href = "/" + (e.target.id);
 });
