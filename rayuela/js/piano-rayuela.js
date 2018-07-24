2547437ab870a069945bf753b0948aa73e19bf3b
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2018-07-24 16:18:41 -0300

    Update piano-rayuela.js

--- rayuela/js/piano-rayuela.js
+++ rayuela/js/piano-rayuela.js
@@ -2,45 +2,45 @@ var piano = Synth.createInstrument('piano');
 $("#1").click(function(){
     piano.play('C', 4, 2);
     var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 });
 $("#2").click(function(){
     piano.play('D', 4, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 });        
 $("#3").click(function(){
     piano.play('E', 4, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 });  
 $("#4").click(function(){
     piano.play('F', 4, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 });  
 $("#5").click(function(){
     piano.play('G', 4, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 });  
 $("#6").click(function(){
     piano.play('A', 4, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 });  
 $("#7").click(function(){
     piano.play('B', 4, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 });  
 $("#8").click(function(){
     piano.play('C', 5, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 }); 
 $("#9").click(function(){
     piano.play('D', 5, 2);
         var color = ($( this ).css( "background-color" ))
-    $(".modal").css("background-color", color);
+    $("body").css("background-color", color);
 }); 
