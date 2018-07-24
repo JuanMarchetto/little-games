cdd07000555a30e7e81aa8fbbf1ac5025c600e2b
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2018-07-24 17:45:55 -0300

    Delete piano-rayuela.js

--- rayuela/js/piano-rayuela.js
+++
@@ -1,5 +0,0 @@
-var piano = Synth.createInstrument('piano');
-$(".tecla").click(function(e){
-    piano.play(($(e.target).attr("key")), ($(e.target).attr("oct")), 2);
-    $("body").css("background-color", ($( this ).css( "background-color" )));
-});
