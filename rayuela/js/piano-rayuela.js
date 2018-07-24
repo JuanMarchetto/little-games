5f997e1bbe2ed421a55af51a7b5fd2bc054b8c50
Juan Patricio Marchetto <juanpatriciomarchetto@gmail.com>
2018-07-24 17:35:34 -0300

    minimizing function

--- rayuela/js/piano-rayuela.js
+++ rayuela/js/piano-rayuela.js
@@ -1,46 +1,5 @@
 var piano = Synth.createInstrument('piano');
-$("#1").click(function(){
-    piano.play('C', 4, 2);
-    var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
+$(".tecla").click(function(e){
+    piano.play(($(e.target).attr("key")), ($(e.target).attr("oct")), 2);
+    $("body").css("background-color", ($( this ).css( "background-color" )));
 });
-$("#2").click(function(){
-    piano.play('D', 4, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-});        
-$("#3").click(function(){
-    piano.play('E', 4, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-});  
-$("#4").click(function(){
-    piano.play('F', 4, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-});  
-$("#5").click(function(){
-    piano.play('G', 4, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-});  
-$("#6").click(function(){
-    piano.play('A', 4, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-});  
-$("#7").click(function(){
-    piano.play('B', 4, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-});  
-$("#8").click(function(){
-    piano.play('C', 5, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-}); 
-$("#9").click(function(){
-    piano.play('D', 5, 2);
-        var color = ($( this ).css( "background-color" ))
-    $("body").css("background-color", color);
-}); 
