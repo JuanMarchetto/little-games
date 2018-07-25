d6150df0fa6a009831426d19a1a9a6530ca9658c
user.name <juanpatriciomarchetto@gmail.com>
2018-07-24 23:30:03 -0300

    optimizing function for reusability

--- js/piano.js
+++ js/piano.js
@@ -1,5 +1,12 @@
-var piano = Synth.createInstrument('piano');
 $(".tecla").click(function(e){
-    piano.play(($(e.target).attr("key")), ($(e.target).attr("oct")), ($(e.target).attr("time")));
-    $(".bg-change").css("background-color", ($( this ).css( "background-color" )));
+	var instrumento = Synth.createInstrument('piano');
+	var time = 2;
+    if (($(e.target).attr("time")) != undefined){
+    	time = ($(e.target).attr("time"));
+    }
+    if (($(e.target).attr("instrumento")) != undefined){
+    	instrumento = ($(e.target).attr("instrumento"));
+    }
+  instrumento.play(($(e.target).attr("key")), ($(e.target).attr("oct")), time);
+  $(".bg-change").css("background-color", ($( this ).css( "background-color" )));
 });
